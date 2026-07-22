05bcfb0bb1d4f890ff4f7eeeec60e6b5ee3bf5df
Mantra <mantrakusumgar@outlook.com>
2026-07-22 14:55:58 +0100

    remove safari

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,10 +42,5 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 });
